d2a03f240c6680aa651f864d695890d41b889a8a
grimorian <web@grimorian.com>
2019-04-22 06:44:22 -0500

    FIXING PATHS

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app = express()
 app.use(express.static('.'))
 
 https.createServer({
-  key: fs.readFileSync('etc/letsencrypt/live/grimorian.com/privkey.pem'),
-  cert: fs.readFileSync('etc/letsencrypt/live/grimorian.com/cert.pem'),
-  ca: fs.readFileSync('etc/letsencrypt/live/grimorian.com/chain.pem')
+  key: fs.readFileSync('/etc/letsencrypt/live/grimorian.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/grimorian.com/cert.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/grimorian.com/chain.pem')
 }, app).listen(443, () => {
   console.log('Grimorian is listening...')
 })
